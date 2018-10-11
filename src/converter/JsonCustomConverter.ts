afea1cd29836b0ff59923d00582b3e6ae89a47b9
henry781 <henry.ernst@hotmail.fr>
2018-10-11 10:29:30 +0200

    remove JsonCustomConverter constructor

--- src/converter/JsonCustomConverter.ts
+++ src/converter/JsonCustomConverter.ts
@@ -3,7 +3,4 @@ export abstract class JsonCustomConverter<T> {
     public abstract serialize(obj: T): any;
 
     public abstract deserialize(obj: any): T;
-
-    protected constructor() {
-    }
 }
