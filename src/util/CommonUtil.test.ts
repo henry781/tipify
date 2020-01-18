0c0c92a5d383d7869d33dcdf8d68952802aba455
henry781 <henry.ernst@hotmail.fr>
2020-01-18 18:35:18 +0100

    test(CommonUtil): add tests

--- src/util/CommonUtil.test.ts
+++ src/util/CommonUtil.test.ts
@@ -1,11 +1,8 @@
 import {expect} from 'chai';
 import {JsonConverterError} from '../core/JsonConverterError';
-import {tryParseBoolean, tryParseNumber} from './CommonUtil';
+import {isBoolean, isNullOrUndefined, isNumber, isObject, isString, tryParseBoolean, tryParseNumber} from './CommonUtil';
 
-/**
- * JsonConverterUtil
- */
-describe('JsonConverterUtil', () => {
+describe('CommonUtil', () => {
 
     /**
      * Try to parse a string to a boolean
@@ -66,6 +63,77 @@ describe('JsonConverterUtil', () => {
         it('should return 12.12 when string is 12.12', () => {
             expect(() => tryParseNumber('something'))
                 .throw(JsonConverterError, 'Obj cannot be parsed to type <Number>');
+        });
+    });
+
+    describe('isNullOrUndefined', () => {
+
+        it('should return true when null', () => {
+            expect(isNullOrUndefined(null)).to.be.true;
+        });
+
+        it('should return true when undefined', () => {
+            expect(isNullOrUndefined(undefined)).to.be.true;
+        });
+
+        it('should return false when 0', () => {
+            expect(isNullOrUndefined(0)).to.be.false;
+        });
+
+        it('should return false when empty string', () => {
+            expect(isNullOrUndefined('')).to.be.false;
+        });
+
+        it('should return false when non empty string', () => {
+            expect(isNullOrUndefined('test')).to.be.false;
+        });
+    });
+
+    describe('isString', () => {
+
+        it('should return false when object', () => {
+            expect(isString({})).to.be.false;
+        });
+
+        it('should return true when string', () => {
+            expect(isString('test')).to.be.true;
+        });
+    });
+
+    describe('isBoolean', () => {
+
+        it('should return false when object', () => {
+            expect(isBoolean({})).to.be.false;
+        });
+
+        it('should return true when boolean', () => {
+            expect(isBoolean(true)).to.be.true;
+        });
+
+        it('should return true when boolean (false)', () => {
+            expect(isBoolean(false)).to.be.true;
+        });
+    });
+
+    describe('isNumber', () => {
+
+        it('should return false when object', () => {
+            expect(isNumber({})).to.be.false;
+        });
+
+        it('should return true when number', () => {
+            expect(isNumber(10)).to.be.true;
+        });
+    });
+
+    describe('isObject', () => {
+
+        it('should return false when number', () => {
+            expect(isObject(10)).to.be.false;
+        });
+
+        it('should return true when object', () => {
+            expect(isObject({})).to.be.true;
         });
     });
 });
